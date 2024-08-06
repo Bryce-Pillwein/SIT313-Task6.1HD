b65d55a7dacf12129f7c7830a249147b45d62813
Bryce <bpillwein23@gmail.com>
2024-08-06 17:29:04 +1000

    fixes: for deploy

--- src/components/stripe/XSubscribeMembership.tsx
+++ src/components/stripe/XSubscribeMembership.tsx
@@ -22,7 +22,7 @@ const SubscribeMembership = ({ priceId, price, description }: Props) => {
     }
 
     try {
-      const response = await fetch('/api/stripe/checkout', {
+      const response = await fetch('/api/stripe-checkout', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -30,9 +30,14 @@ const SubscribeMembership = ({ priceId, price, description }: Props) => {
         body: JSON.stringify({ priceId: priceId }),
       });
 
-      if (!response.ok) throw new Error('Something went wrong');
+      if (!response.ok) {
+        console.log("RESPONSE ERROR")
+        console.log(response);
+        throw new Error('Something went wrong');
+      }
 
       const data = await response.json();
+      console.log("Data in subscirbe", data);
       await stripe.redirectToCheckout({ sessionId: data.result.id, });
 
     } catch (error) {
@@ -41,11 +46,25 @@ const SubscribeMembership = ({ priceId, price, description }: Props) => {
   };
 
   return (
-    <div>
-      Click Below button to get {description}
-      <button onClick={handleSubmit}>
-        Upgrade in {price}
-      </button>
+    <div className="flex justify-evenly">
+
+      {/* Free Membership */}
+      <div>
+        <h1>Free Membership</h1>
+
+      </div>
+
+      {/* Premium Membership */}
+      <div>
+        <h1>Premium Membership</h1>
+
+        <p>Product: {description}</p>
+        <p>${price} AUD</p>
+
+        <button type="button" onClick={handleSubmit}
+          className='bg-hsl-l50 font-medium font-sdisplay px-4 py-2 rounded-lg'>UPGRADE</button>
+      </div>
+
     </div>
   );
 };
